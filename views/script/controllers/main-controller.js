060c5787cf77374e640d146149260ac03ee879dc
milotiger <black.thanhtri@gmail.com>
2016-10-24 10:58:02 +0700

    update ui

--- views/script/controllers/main-controller.js
+++ views/script/controllers/main-controller.js
@@ -1,25 +1,49 @@
 (function () {
 	var app = angular.module('mainApp');
-	app.controller('mainCtr', function($scope, $rootScope, uiService, $http){
+	app.controller('mainCtr', function($scope, $rootScope, uiService, $http, $timeout){
 		window.sc = $scope;
 		sc.step = 4;
+		sc.isWait = false;
 
 		sc.infor = {};
 		sc.infor.customer = {};
+		sc.infor.detail = {};
 
-		sc.AllAirports = [];
+		sc.allAirports = [];
 
 		sc.initInput = function initInput() {
 			uiService.initInput();
 		}
 
+		sc.initInputDelay = function initInputDelay(delay) {
+			if (!delay)
+				delay = 100;
+			$timeout(function(){
+				sc.initInput();
+			}, delay);
+		}
+
 		sc.loadAllAirports = function loadAllAirports() {
+			Wait();
 			$http.get($rootScope.baseApi + '/flights')
 			.success(function (data) {
+				unWait();
+				sc.allAirports = data;
 				console.log(data);
+				sc.initInputDelay();
 			})
 		}
 
 		sc.loadAllAirports();
+
+		function Wait() {
+			sc.Wait = true;
+			sc.$evalAsync();
+		}
+
+		function unWait() {
+			sc.Wait = false;
+			sc.$evalAsync();
+		}
 	});
 })();
